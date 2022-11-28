668f695caaac783aecf06a5bb94561c56d0e6820
yuck <60288171+yuckdevchan@users.noreply.github.com>
2022-11-28 13:01:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ print("Welcome to Fresh Redux Lyric Generator")
 name = input("Insert name of person: ")
 pronoun = input("Insert pronoun of person: ")
 parent = input("Insert parent of person: ")
+parentpronoun = input("Insert parent's pronoun: ")
 plate = input("Insert license plate of car: ")
 place = input("Where is your person going? ")
 placenoun = input("Is your place a proper noun? (y, n): ")
+born = input("Where was your person born?: ")
+days = input("Where did they spend most of their days: ")
 title = input("Insert what they're going to become when they get there: ")
+drinkglass = input("What does your person drink out of")
 
 if placenoun == "y":
     placenoun = "the"
@@ -21,46 +25,48 @@ elif placenoun == "n":
 else:
     print("Error: Invalid Answer")
     sys.exit
+    
+place = place + placenoun
 
-lyrics = """Now, this is a story all about how
+lyrics = f"""Now, this is a story all about how
 Matty E's life got flipped-turned upside down
 And I'd like to take a minute
 Just sit right there
-I'll tell you how Matthew became the bozo of a town called Bel-Air
-In Alberquerque, New Mexico born and raised
-In the crackden was where Matty spent most of his days
+I'll tell you how {name} became the {title} of a town called {place}
+In {born} born and raised
+In the {days} was where {name} spent most of his days
 Chillin' out, maxin', relaxin', all cool
 And all shootin' some b-ball outside of the school
 When a couple of guys who were up to no good
 Started making trouble in his neighborhood
-He got in one little fight and Stuart got scared
-He said, "You're movin' with your auntie and uncle in Bel-Air"
-Matty begged and pleaded with him day after day
-But he packed Matty's suitcase and sent him on his way
+{pronoun} got in one little fight and {parent} got scared
+{pronoun} said, "You're movin' with your auntie and uncle in {place}"
+{name} begged and pleaded with {parentpronoun} day after day
+But he packed {name}'s suitcase and sent him on his way
 He gave him a kiss and then he gave him his ticket
 Matty put his Walkman on and said, "I might as well kick it"
 First class, yo this is bad
-Drinking orange juice out of a crushed coke can
-Is this what the people in jail living like?
+Drinking orange juice out of a {drinkglass} can
+Is this what the people in {place} living like?
 Hmm, this might be alright
 But wait, I hear they're prissy, bourgeois, all that
 Is this the type of place that they just send this cool cat?
 I don't think so
-He'll see when he gets there
-Matty hopes that they're prepared for the bozo of county jail
-Well, the plane landed and when he came out
+{pronoun}'ll see when he gets there
+{name} hopes that they're prepared for the {title} of {place}
+Well, the plane landed and when {pronoun} came out
 There was a dude who looked like a cop standing there with my name out
-Matty ain't trying to get arrested yet, he just got here
-he sprang with the quickness like lightning, disappeared
-he whistled for a cab and when it came near
-The license plate said, "CRAZY" and it had dice in the mirror
-If anything he could say that this cab was rare
-But he thought "Nah, forget it, yo, holmes to Bel Air"
+{name} ain't trying to get arrested yet, {pronoun} just got here
+{pronoun} sprang with the quickness like lightning, disappeared
+{pronoun} whistled for a cab and when it came near
+The license plate said, "{plate}" and it had dice in the mirror
+If anything {pronoun} could say that this cab was rare
+But {pronoun} thought "Nah, forget it, yo, holmes to Bel Air"
 Matty pulled up to the house about seven or eight
 And he yelled to the cabbie, "Yo holmes, smell ya later"
-He looked at the prison
-He was finally there
-To sit in his cell as the bozo of County Jail"""
+{pronoun} looked at the {place}
+{pronoun} was finally there
+To sit in their cell as the {title} of {place}"""
 
 clipboard(lyrics)
 print(lyrics)
